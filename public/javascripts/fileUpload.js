c5961fe90e3f3082a7c3873159d459ba7a56ca58
Seungjun Chae <sjchae@sjchae-Gram17.localdomain>
2023-01-30 03:36:50 -0500

    finish project

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,13 +1,34 @@
-FilePond.registerPlugin(
-	FilePondPluginImagePreview,
-	FilePondPluginImageResize,
-	FilePondPluginFileEncode
-);
+const rootStyles = window.getComputedStyle(document.documentElement);
 
-FilePond.setOptions({
-	stylePanelAspectRatio: 150 / 100,
-	imageResizeTargetWidth: 100,
-	imageResizeTargetHeight: 150,
-});
+if (
+	rootStyles.getPropertyValue('--book-cover-width-large') != null &&
+	rootStyles.getPropertyValue('--book-cover-width-large') != ''
+) {
+	ready();
+} else {
+	document.getElementById('main-css').addEventListener('load', ready);
+}
 
-FilePond.parse(document.body);
+function ready() {
+	const coverAspectRatio = parseFloat(
+		rootStyles.getPropertyValue('--book-cover-width-ratio')
+	);
+	const coverWidth = parseFloat(
+		rootStyles.getPropertyValue('--book-cover-width-large')
+	);
+	const coverHeight = parseFloat(coverWidth / coverAspectRatio);
+
+	FilePond.registerPlugin(
+		FilePondPluginImagePreview,
+		FilePondPluginImageResize,
+		FilePondPluginFileEncode
+	);
+
+	FilePond.setOptions({
+		stylePanelAspectRatio: coverAspectRatio,
+		imageResizeTargetWidth: coverWidth,
+		imageResizeTargetHeight: coverHeight,
+	});
+
+	FilePond.parse(document.body);
+}
