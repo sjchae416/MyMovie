d1d2667c219041e90cf0fa73052f3dad004dcb10
Seungjun <seungjun.chae@stonybrook.edu>
2023-02-01 23:59:12 -0500

    changed server port

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.use('/', indexRouter);
 app.use('/actors', actorRouter);
 app.use('/movies', movieRouter);
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 8000, () => {
 	console.log(`Server running on port: ${process.env.PORT}`);
 });
